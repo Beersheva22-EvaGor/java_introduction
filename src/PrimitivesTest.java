05af8766b19ec5a729e84aeb3057d3c7446d6065
eva-gor <evagor82@gmail.com>
2022-10-29 07:34:57 +0300

    Homework. Primitive operations: Get/Set/Revert bit Value

--- src/PrimitivesTest.java
+++ src/PrimitivesTest.java
@@ -1,55 +1,43 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class PrimitivesTest {
 
-	@Test 
-	@Disabled
-	void operatorTest() {	
-		int number = 123;
-		/*
-		 * assertEquals(3, getThirdDigit(number)); 
-		 * assertEquals(2,getSecondDigit(number)); 
-		 * assertEquals(1, getFirstDigit(number));
-		 */
-		assertEquals(3, getDigit(number, 3));
-		assertEquals(2, getDigit(number, 2));
-		assertEquals(1, getDigit(number, 1));
-		
-	}
-	// Primitive method
-	/*
-	 * private int getFirstDigit(int number) { return getThirdDigit((number -
-	 * getSecondDigit(number))/100); }
-	 * 
-	 * private int getSecondDigit(int number) { return getThirdDigit((number -
-	 * getThirdDigit(number))/10); }
-	 * 
-	 * private int getThirdDigit(int number) { return number%10; }
-	 */
-	
-	// Recursive method
-	/* For getting digit you should call the method with two arguments:
-	 * First - is a number
-	 * Second - is a position of a digit you want to get (counting starts from the number 1: 
-	 * if number is 123 then to get 3rd number you need to call getDigit(123,3)
-	 * NB. There's no comparing the position to the real length of a number*/
-	private int getDigit(int number, int pos)
-	{
-		int result = number;
-		int length_number = Integer.toString(number).length();
-		for (var i=0; i<length_number-pos;i++)
-		{
-			result = result - getDigit(number, length_number - i)*(int)Math.pow(10,i);		
-		}
-		
-		return result/(int)Math.pow(10,length_number - pos) %10;
-	}
-
+	/* NB. Indexing in a method's arguments starts with 0! Therefore the least bit has 0 position */
 	@Test
-	void newTest() {
+	void getBitValueTest() {
+		long number = 0x3ab7f5; //11101010110_1_11111_1_0101
+
+		assertEquals(1, BitOperations.getBitValue(number, 5));
+		assertEquals(0, BitOperations.getBitValue(number, 11));
+		assertEquals(1, BitOperations.getBitValue(number, 2));
+		assertEquals(0, BitOperations.getBitValue(number, 1));
+		assertEquals(-1, BitOperations.getBitValue(number, 100));
+		assertEquals(-1, BitOperations.getBitValue(number, -2));
+	}
+	
+	@Test
+	void setBitValueTest()
+	{
+		long number = 0x3ab7f5;													//11101010110111111_1_0101
+				
+		// !In a source code here was a mistake: nBit = 4 (not 5). To check it uncomment next line
+		// System.out.println("0x3ab7e5 = "+ Long.toBinaryString(0x3ab7e5));
+		assertEquals(0x3ab7e5, BitOperations.setBitValue(number, 4, false));	// 11101010110111111_0_0101
+		assertEquals(0x3ab7f5, BitOperations.setBitValue(number, 5, true));		// 1110101011011111_1_00101
 		
+	}
+	
+	@Test 
+	void revertBitValueTest()
+	{
+		long number = 0x3ab7f5; 												//001110101011011111_1_10101
+		
+		// !In a source code here was a mistake: 0x3ab7d5 instead of 0x3ab7e5. To check it uncomment next line
+		// System.out.println("0x3ab7e5 = "+ Long.toBinaryString(0x3ab7e5));  	// 11101010110111111_0_0101
+		assertEquals(0x3ab7d5, BitOperations.revertValue(number, 5));			//001110101011011111_0_10101
+		assertEquals(0x3ab7f4, BitOperations.revertValue(number, 0));			//00111010101101111111010_0
 	}
 }
