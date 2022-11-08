d0ef99ef591c25773d3cf60b2af60e4b21d11b8a
eva-gor <evagor82@gmail.com>
2022-11-08 14:07:55 +0200

    HW2: end-var

--- src/Numbers.java
+++ src/Numbers.java
@@ -33,11 +33,13 @@ public class Numbers {
 			oper=0;			
 			for (int i = 1; i<=length; i++ )
 			{ 			
-				oper = oper + ((int)(number / Math.pow(10, length-i)) %10 )*(i<=3? 1:-1);
+				oper = oper + ((int)(number / Math.pow(10, length-i)) %10 )*(i<=nDigits/2? 1:-1);
 			}
 			
 		}
 		
 		return oper == 0;
 	}
+	
+	
 }
